3b067fae7f1043b0d88ef4459f6479d958258e6e
Raphael E <raphaelsebastien.evangelista@gmail.com>
2025-08-25 21:49:13 -0500

    implemented image carousel in homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
+import HomePage from './customer/components/Navigation/Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <div>
+        <HomePage/>
+      </div>
     </div>
   );
 }
---
+++ src/customer/components/Navigation/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from '../../HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCarousel/>
+
+            <div>
+                Other Section
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
