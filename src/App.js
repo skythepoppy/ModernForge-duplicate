4516997fd9c47037f22d22c8c508f43ca76b1723
Raphael E <raphaelsebastien.evangelista@gmail.com>
2025-08-25 21:23:12 -0500

    added navbar with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Navigation/>
     </div>
   );
 }
