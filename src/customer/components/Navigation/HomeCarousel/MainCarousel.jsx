e76577bd2dffa0d86431ecda106b7895a73bfeef
Raphael E <raphaelsebastien.evangelista@gmail.com>
2025-08-25 22:49:58 -0500

    added static homepage images and resized

--- src/customer/components/Navigation/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/Navigation/HomeCarousel/MainCarousel.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import MainCarouselData from './MainCarouselData';
 
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
+const MainCarousel = () => {
+    const items = MainCarouselData.map((item) => (
+        <img 
+            key={item.id}
+            className="cursor-pointer w-full h-[60vh] object-cover"
+            role="presentation"
+            src={item.imageUrl}
+            alt={item.title} 
+        />
+    ));
 
-const MainCarousel = () => (
-    <AliceCarousel
-        animationType="fadeout" 
-        animationDuration={800}
-        disableButtonsControls
-        infinite
-        items={items}
-        mouseTracking
-    />
-);
+    return (
+        <div className="w-full mx-auto">
+            <AliceCarousel
+                animationType="fadeout"
+                animationDuration={800}
+                disableButtonsControls
+                infinite
+                items={items}
+                mouseTracking
+                autoPlay
+                autoPlayInterval={3000}
+            />
+        </div>
+    );
+};
 
-export default MainCarousel
+export default MainCarousel;
---
+++ src/customer/components/Navigation/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,34 @@
+import statictoy1 from './homeassets/statictoy1.jpg';
+import statictoy2 from './homeassets/statictoy2.jpg';
+import statictoy3 from './homeassets/statictoy3.jpg';
+import statictoy4 from './homeassets/statictoy4.jpg';
+
+
+const MainCarouselData = [
+  {
+    id: 1,
+    title: "Example Toy 1",
+    imageUrl: statictoy1,
+    
+  },
+  {
+    id: 2,
+    title: "Example Toy 2",
+    imageUrl: statictoy2,
+    
+  },
+  {
+    id: 3,
+    title: "Example Toy 3",
+    imageUrl: statictoy3,
+    
+  },
+  {
+    id: 4,
+    title: "Example Toy 4",
+    imageUrl: statictoy4,
+    
+  },
+];
+
+export default MainCarouselData;
